53448790de0203966c72a42bf8d8436aa757d40b
jaeminjo732 <jaeminjo732@gmail.com>
2025-12-14 13:35:00 +0900

    feat :: JWT 인증 설정 구현
    - JwtConfig 추가
    - RsaKeyProperties 구성
    
    Co-Authored-By: jaeminjo732 <jaeminjo732@gmail.com>

---
+++ src/main/java/gc/demo/config/JwtConfig.java
@@ -0,0 +1,46 @@
+package gc.demo.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
+@EnableConfigurationProperties(RsaKeyProperties.class)
+public class JwtConfig {
+    private final RsaKeyProperties rsaKeys;
+
+    public JwtConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey())
+                .privateKey(rsaKeys.privateKey())
+                .build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public JWKSet jwkSet() {
+        RSAKey rsaKey = new RSAKey.Builder(rsaKeys.publicKey())
+                .build();
+        return new JWKSet(rsaKey);
+    }
+}
